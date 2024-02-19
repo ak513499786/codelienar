8f3f43a8c85981f63188f2fcd98a633938cbe29c
Md Ahmed Ali Khan <79787782+ak513499786@users.noreply.github.com>
2024-02-19 18:49:10 +0530

    Update controller.js

--- ctrl/controller.js
+++ ctrl/controller.js
@@ -1,1 +1,36 @@
 
+const nodemailer= require("nodemailer");
+var validator = require("node-email-validation");
+const { response } = require("express");
+
+const sendMailContact = (req, res) => {
+    const { username, lastname, email, message } = req.body;
+    if(validator.is_email_valid(email)){
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "ahmedkhan.cse010@gmail.com",
+          pass: "dpyq uhvd jlgj tarb",
+        },
+      });
+      const mailOptions = {
+        from: { email },
+        to: "ahmed@codelinear.com",
+        subject: "New message from your website",
+        text: `FirstName: ${username}\nLastName: ${lastname}\nEmail: ${email}\nMessage: ${message}`,
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send("Error sending email");
+        } else {
+          console.log("Email sent:", info.response);
+          res.status(200).send("Email sent successfully");
+        }
+      });
+    }
+    else{
+      res.status(500).send("Error sending email");
+    }
+  };
+  module.exports =  {sendMailContact}
