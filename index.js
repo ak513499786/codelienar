f616387df3a19dfdfe320914c4fdf3102fa6edea
Md Ahmed Ali Khan <79787782+ak513499786@users.noreply.github.com>
2024-05-08 14:43:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express= require("express");
-const cors= require("cors");
+const express = require("express");
+const cors = require("cors");
 const { router } = require("./routes/routes");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(router)
+app.use(router);
+
 const port = process.env.PORT || 4500;
-app.listen(port, () => {
-  console.log(`app running on port ${port}...`);
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
 });
