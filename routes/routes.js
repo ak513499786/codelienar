1ae8bf8d687a86c90f695ff5c61a0e5684600d26
Md Ahmed Ali Khan <79787782+ak513499786@users.noreply.github.com>
2024-05-08 14:43:23 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
-const express= require("express");
+const express = require("express");
 const { sendMailContact } = require("../ctrl/controller");
 
 const router = express.Router();
-router.post("/", sendMailContact);
-module.exports = {router}
+
+router.post("/sendMailContact", sendMailContact);
+module.exports = { router };
