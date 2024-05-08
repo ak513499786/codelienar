988b6e18c92c1633484b50341cd00beb424469e6
Md Ahmed Ali Khan <79787782+ak513499786@users.noreply.github.com>
2024-05-08 14:43:03 +0530

    Update controller.js

--- ctrl/controller.js
+++ ctrl/controller.js
@@ -1,36 +1,35 @@
-const nodemailer= require("nodemailer");
+const nodemailer = require("nodemailer");
 var validator = require("node-email-validation");
-const { response } = require("express");
 
 const sendMailContact = (req, res) => {
-    const { username, lastname, email, message } = req.body;
-    console.log('happy');
-    if(validator.is_email_valid(email)){
-      const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: "developer@codelinear.com",
-          pass: "umlw qvhn nslr udrt",
-        },
-      });
-      const mailOptions = {
-        from: { email },
-        to: "info@codelinear.com",
-        subject: "New message from your website",
-        text: `FirstName: ${username}\nLastName: ${lastname}\nEmail: ${email}\nMessage: ${message}`,
-      };
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.error(error);
-          res.status(500).send("Error sending email");
-        } else {
-          console.log("Email sent:", info.response);
-          res.status(200).send("Email sent successfully");
-        }
-      });
-    }
-    else{
-      res.status(500).send("Error sending email");
-    }
-  };
-  module.exports =  {sendMailContact}
+  const { username, lastname, email, message } = req.body;
+  if (validator.is_email_valid(email)) {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "developer@codelinear.com",
+        pass: "umlw qvhn nslr udrt",
+      },
+    });
+    const mailOptions = {
+      from: "Codelinear <developer@codelinear.com>",
+      to: "ahmed@codelinear.com",
+      subject: "New message from your website",
+      text: `Name: ${username} ${lastname}\nEmail: ${email}\n\nMessage: ${message}`,
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send("Error sending email");
+      } else {
+        console.log("Email sent:", info.response);
+        res.status(200).send("Email sent successfully");
+      }
+    });
+  } else {
+    res.status(500).send("Error sending email");
+  }
+};
+module.exports = {
+  sendMailContact,
+};
